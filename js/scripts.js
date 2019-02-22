fbb43f12b9c41bebd3dc9972057df6399b07677e
Sowayi <sapwao@gmail.com>
2019-02-22 10:14:36 -0500

    Add conditional statements to check user input

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+var counter = 0;
+if (question1 === "a"){
+  counter += 5;
+}
+if (question2 === "a"){
+  counter += 5;
+}
+if (question3 === "b"){
+  counter += 5;
+}
+if (question4 === "a"){
+  counter += 5;
+}
+if (question5 === "c"){
+  counter += 5;
+}
+if (question6 === "b"){
+  counter += 5;
+}
+if (question7 === "d"){
+  counter += 5;
+}
